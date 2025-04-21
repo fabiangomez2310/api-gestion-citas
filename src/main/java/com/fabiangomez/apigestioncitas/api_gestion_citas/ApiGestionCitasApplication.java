e080d4e9ac404e42db6d6800df184fe74683cfdf
FABIAN GOMEZ <fabian.gomez.2310@gmail.com>
2025-04-21 17:53:01 -0500

    Inicio del proyecto

---
+++ src/main/java/com/fabiangomez/apigestioncitas/api_gestion_citas/ApiGestionCitasApplication.java
@@ -0,0 +1,13 @@
+package com.fabiangomez.apigestioncitas.api_gestion_citas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestionCitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGestionCitasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabiangomez/apigestioncitas/api_gestion_citas/ApiGestionCitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabiangomez.apigestioncitas.api_gestion_citas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestionCitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
